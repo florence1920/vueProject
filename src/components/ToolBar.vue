4f1afd4bbbbb428cc5c90e89eae7c07bb4b426ac
florence1920 <jetaime12121@gmail.com>
2021-07-17 23:00:24 +0900

    commit test

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">VUE</a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link to="/news" class="nav-link">News</router-link> 
+                    </li>
+                    <li>
+                        <router-link to="/ask" class="nav-link">Ask</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/jobs" class="nav-link">Jobs</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+
+</style>
---
+++ src/views/JobsView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div v-for="job in jobs" v-bind:key="job.id">{{job.title}}</div>
+        
+    </div>
+</template>
+
+<script>
+import { fetchJobList } from '../api/index.js';
+export default {
+    data(){
+        return{
+            jobs:[]
+        }
+    },
+    created(){
+        fetchJobList()
+        .then(response => this.jobs = response.data)
+        .catch(error => console.log(error));
+    }
+}
+</script>
+
+<style>
+
+</style>
